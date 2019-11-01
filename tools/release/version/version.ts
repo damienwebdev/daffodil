e44b6b7a9c3d0b57c890e85ab669a2c6790a50b4
Damien Retzinger <damienwebdev@gmail.com>
2019-11-01 09:59:31 -0400

    build(release): remove duplicate commit task

--- tools/release/version/version.ts
+++ tools/release/version/version.ts
@@ -1,10 +1,8 @@
 import { series } from 'gulp';
 import { rootVersion } from './root-version';
 import { leafVersion } from './leaf-version';
-import { commitChangelogAndTag } from './generate-commit';
 
 export const version = series(
   rootVersion, 
   leafVersion,
-  commitChangelogAndTag
 ); 
