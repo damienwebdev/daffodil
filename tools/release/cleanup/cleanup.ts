04b51491957a06b0e40eb6fb60e114aaeabbb2cf
Damien Retzinger <damienwebdev@gmail.com>
2019-11-01 11:37:57 -0400

    tmp

--- tools/release/cleanup/cleanup.ts
+++ tools/release/cleanup/cleanup.ts
@@ -2,8 +2,12 @@ import { series } from 'gulp';
 import * as git from 'simple-git/promise';
 import { RELEASE_CONFIG } from '../config';
 
-export const cleanup = async() => {
+export const removeTemporaryBranch = async() => {
   const repo = await git(RELEASE_CONFIG.PROJECT_PATH);
   await repo.checkout(RELEASE_CONFIG.BASE_BRANCH);
   await repo.deleteLocalBranch(RELEASE_CONFIG.TEMPORARY_BRANCH_NAME);
 }
+
+export const cleanup = series(
+  removeTemporaryBranch
+)
