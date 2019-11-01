d77b003a582159cdeb9843f41a72c7e92e8552bf
Damien Retzinger <damienwebdev@gmail.com>
2019-11-01 11:30:03 -0400

    tmp

--- tools/release/prepare/prepare.ts
+++ tools/release/prepare/prepare.ts
@@ -3,26 +3,6 @@ import { parallel, series } from 'gulp';
 import { clean } from './clean';
 import * as git from 'simple-git/promise';
 
-const errorOnExistingBranch = (branchName: string) => (result: boolean) : void => {
-  if(result) {
-    throw(`You have a local copy of the "${branchName}" branch, please delete it before proceeding.`)
-  }
-}
-
-const branchExists = async (branchName: string): Promise<boolean> => {
-    const repo = await git(RELEASE_CONFIG.PROJECT_PATH);
-    const branches = await repo.branchLocal();
-    return branches[branchName] !== undefined;
-}
-
-const verifyDevelopBranch = () : Promise<void> => {
-  return branchExists("develop").then(errorOnExistingBranch("develop"));
-}
-
-const verifyMasterBranch = () : Promise<void> => {
-  return branchExists("master").then(errorOnExistingBranch("master"));
-}
-
 /**
  * Function which determines whether or not the current workspace is dirty (git).
  */
@@ -47,10 +27,6 @@ const checkoutReleaseBranch = async () => {
 
 export const prepare = series(
   checkoutReleaseBranch,
-  parallel(
-    verifyDevelopBranch, 
-    verifyMasterBranch, 
-    verifyWorkspace
-  ),
+  verifyWorkspace,
   clean
 );
