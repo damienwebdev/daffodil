59c04f0e2ddb9fd16107ec337c8713818b14820f
Damien Retzinger <damienwebdev@gmail.com>
2019-11-01 12:14:02 -0400

    tmp

--- tools/release/cleanup/cleanup.ts
+++ tools/release/cleanup/cleanup.ts
@@ -5,7 +5,7 @@ import { RELEASE_CONFIG } from '../config';
 const removeTemporaryBranch = async() => {
   const repo = await git(RELEASE_CONFIG.PROJECT_PATH);
   await repo.checkout(RELEASE_CONFIG.GIT_REMOTE_NAME + '/' + RELEASE_CONFIG.BASE_BRANCH);
-  await repo.branch(['-D ' + RELEASE_CONFIG.TEMPORARY_BRANCH_NAME]);
+  await repo.branch(['-D ', RELEASE_CONFIG.TEMPORARY_BRANCH_NAME]);
 }
 
 export const cleanup = series(
